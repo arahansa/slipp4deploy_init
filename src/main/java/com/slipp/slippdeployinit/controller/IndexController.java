dcc73bc3fefd4865f63841e998b86711a53c2bd9
arahansa <arahansa@naver.com>
2017-08-30 21:23:52 +0900

    add .

--- src/main/java/com/slipp/slippdeployinit/controller/IndexController.java
+++ src/main/java/com/slipp/slippdeployinit/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.slipp.slippdeployinit.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+    @Value("${msg}")
+    private String msg;
+
     @GetMapping("/")
     @ResponseBody
     public String index(){
-        return "hello world";
+        return "hello world? :"+msg;
     }
 
     @GetMapping("/index")
